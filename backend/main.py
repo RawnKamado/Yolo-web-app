61c5884c4c0fa15bb072e68e6130326b7d8006ef
Bao Quoc <baoquocchthptst@gmail.com>
2026-09-12 03:21:46 +0700

    Docker done

--- backend/main.py
+++ backend/main.py
@@ -78,7 +78,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+    	"http://localhost:5173",
+    	"http://localhost:8080",
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
